bc82e94c8f55f1139496593dfab3cce82c34f60a
Jacob Delgado <jacob.delgado@gmail.com>
2016-07-06 18:26:47 -0600

    Setup project.
    * Add MIT license and CONTRIBUTORS file.
    * Main method.

---
+++ notary.go
@@ -0,0 +1,9 @@
+//
+// Copyright (c) Jacob Delgado. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+
+package main
+
+func main() {
+}
