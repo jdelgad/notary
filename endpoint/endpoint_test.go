d081b629b9806c655accf5c24433bcd1ac982b63
Jacob Delgado <jacob.delgado@gmail.com>
2016-07-06 23:36:33 -0600

    Add MIT license header to all source code and unit tests.

--- endpoint/endpoint_test.go
+++ endpoint/endpoint_test.go
@@ -1,3 +1,8 @@
+//
+// Copyright (c) Jacob Delgado. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+
 package endpoint
 
 import (
